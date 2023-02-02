f43f8ede4077aa75133141f19a81ef126ca7530a
Jacky Ma <303536@qq.com>
2023-02-03 07:39:52 +0800

    chatgptapi

--- src/wechaty/sendMessage.js
+++ src/wechaty/sendMessage.js
@@ -1,5 +1,5 @@
-// import { getChatGPTReply as getReply } from '../chatgpt/index.js'
-import { getOpenAiReply as getReply } from '../openai/index.js'
+import { getChatGPTReply as getReply } from '../chatgpt/index.js'
+//import { getOpenAiReply as getReply } from '../openai/index.js'
 import { botName, roomWhiteList, aliasWhiteList } from '../../config.js'
 
 /**
@@ -21,17 +21,26 @@ export async function defaultMessage(msg, bot) {
   const isRoom = roomWhiteList.includes(roomName) && content.includes(`${botName}`) // 是否在群聊白名单内并且艾特了机器人
   const isAlias = aliasWhiteList.includes(remarkName) || aliasWhiteList.includes(name) // 发消息的人是否在联系人白名单内
   const isBotSelf = botName === remarkName || botName === name // 是否是机器人自己
+  const isNewConversation = content.includes(`新话题`) && content.includes(`${botName}`)// 是否包含机器人 
   // TODO 你们可以根据自己的需求修改这里的逻辑
   if (isText && !isBotSelf) {
     try {
       // 区分群聊和私聊
       if (isRoom && room) {
-        await room.say(await getReply(content.replace(`${botName}`, '')))
+        if(isNewConversation)
+        {
+          const reply = await getReply(content.replace(`${botName}`, ''))
+        }
+        {
+          const reply = await getReply(content.replace(`${botName}`, ''))
+        }
+        await room.say(reply.text)
         return
       }
       // 私人聊天，白名单内的直接发送
       if (isAlias && !room) {
-        await contact.say(await getReply(content))
+        const reply = await getReply(content.text)
+        await contact.say(reply.text)
       }
     } catch (e) {
       console.error(e)
