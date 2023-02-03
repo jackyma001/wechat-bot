13c58555ccd6b8fc67da06b006a1ce0dea52e09b
root <root@ENUITCNYUETAO.ACROTEQ.TX>
2023-02-03 11:45:06 +0800

    install script

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
+// 真实微信名
+export const botWechatName = ''
+
 // 定义机器人的名称，这里是为了防止群聊消息太多，所以只有艾特机器人才会回复，
-export const botName = '助理'
+export const botName = ''
 
 // 群聊白名单，白名单内的群聊才会自动回复
-export const roomWhiteList = ['我们都是伏拉夫','奇迹暖暖家族']
+export const roomWhiteList = []
 
 // 联系人白名单，白名单内的联系人才会自动回复
-export const aliasWhiteList = ['马小宝', '张三', '李四']
+export const aliasWhiteList = []
--- install.sh
+++ install.sh
@@ -25,28 +25,28 @@ echo "Copying the service file..."
 sudo cp chatgpt.service /etc/systemd/system/
 
 # Prompt user to input API key
-read -r "Enter API key: " api_key
+read -p "Enter API key: " api_key
 
 # Replace OPENAI_API_KEY in .env with user's input
 sed -i "s/OPENAI_API_KEY=''/OPENAI_API_KEY='$api_key'/g" .env
 
 # Prompt user to input bot name
-read -r "Enter bot name: " bot_name
+read -p "Enter bot name: " bot_name
 
 # Replace botName in config.js with user's input
-sed -i "s/export const botName = ''/export const botName = '$bot_name'/g" src/config.js
+sed -i "s/export const botName = ''/export const botName = '$bot_name'/g" config.js
 
 # Prompt user to input room white list
-read -r "Enter room white list (format: 'A','B'): " room_white_list
+read -p "Enter room white list (format: 'A','B'): " room_white_list
 
 # Replace roomWhiteList in config.js with user's input
-sed -i "s/export const roomWhiteList = \[\]/export const roomWhiteList = \[$room_white_list\]/g" src/config.js
+sed -i "s/export const roomWhiteList = \[\]/export const roomWhiteList = \[$room_white_list\]/g" config.js
 
 # Prompt user to input alias white list
-read -r "Enter alias white list (format: 'A','B'): " alias_white_list
+read -p "Enter alias white list (format: 'A','B'): " alias_white_list
 
 # Replace aliasWhiteList in config.js with user's input
-sed -i "s/export const aliasWhiteList = \[\]/export const aliasWhiteList = \[$alias_white_list\]/g" src/config.js
+sed -i "s/export const aliasWhiteList = \[\]/export const aliasWhiteList = \[$alias_white_list\]/g" config.js
 
 
 # Reload the system manager configuration
