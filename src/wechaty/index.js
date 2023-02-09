298366d232e30659a54f6956ffd6dbd9dcae406c
jackyma001 <303536@qq.com>
2023-02-09 18:06:25 +0800

    Update index.js

--- src/wechaty/index.js
+++ src/wechaty/index.js
@@ -1,76 +1,23 @@
-import { WechatyBuilder, ScanStatus, log } from 'wechaty'
-import qrTerminal from 'qrcode-terminal'
-import { defaultMessage, shardingMessage } from './sendMessage.js'
-// 扫码
-function onScan(qrcode, status) {
-  if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
-    // 在控制台显示二维码
-    qrTerminal.generate(qrcode, { small: true })
-    const qrcodeImageUrl = ['https://api.qrserver.com/v1/create-qr-code/?data=', encodeURIComponent(qrcode)].join('')
-    console.log('onScan:', qrcodeImageUrl, ScanStatus[status], status)
-  } else {
-    log.info('onScan: %s(%s)', ScanStatus[status], status)
-  }
-}
+import { getChatGPTReply as getReply } from '../chatgpt/index.js'
+import * as readline from "readline"
 
-// 登录
-function onLogin(user) {
-  console.log(`${user} has logged in`)
-  const date = new Date()
-  console.log(`Current time:${date}`)
-  console.log(`Automatic robot chat mode has been activated`)
-}
 
-// 登出
-function onLogout(user) {
-  console.log(`${user} has logged out`)
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// 收到好友请求
-async function onFriendShip(friendship) {
-  const frienddShipRe = /chatgpt|chat/
-  if (friendship.type() === 2) {
-    if (frienddShipRe.test(friendship.hello())) {
-      await friendship.accept()
-    }
-  }
-}
+let conversationId = '';
+let id = '';
+var  recursiveAsyncReadLine = async ()=> {
+  rl.question('Command: ', async (input) => {
+    if (input == 'exit') //we need some base case, for recursion
+      return rl.close(); //closing RL and returning from function.
+    const reply = await getReply(input, conversationId, id)
+    conversationId = reply.conversationId;
+    id = reply.id;
+    recursiveAsyncReadLine(); //Calling this function again to ask new question
+  });
+};
 
-/**
- * 消息发送
- * @param msg
- * @param isSharding
- * @returns {Promise<void>}
- */
-async function onMessage(msg) {
-  // 默认消息回复
-  await defaultMessage(msg, bot)
-  // 消息分片
-  // await shardingMessage(msg,bot)
-}
-
-// 初始化机器人
-export const bot = WechatyBuilder.build({
-  name: 'WechatEveryDay',
-  puppet: 'wechaty-puppet-wechat4u', // 如果有token，记得更换对应的puppet
-  puppetOptions: {
-    uos: true,
-  },
-})
-
-// 扫码
-bot.on('scan', onScan)
-// 登录
-bot.on('login', onLogin)
-// 登出
-bot.on('logout', onLogout)
-// 收到消息
-bot.on('message', onMessage)
-// 添加好友
-bot.on('friendship', onFriendShip)
-
-// 启动微信机器人
-bot
-  .start()
-  .then(() => console.log('Start to log in wechat...'))
-  .catch((e) => console.error(e))
+await recursiveAsyncReadLine(); //we have to actually start our recursion somehow
